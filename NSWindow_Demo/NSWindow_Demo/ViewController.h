41aa0c3a4218f680c42c97e9aee4cc10cc4c8411
LeeWongSnail <wangli_0632@163.com>
2018-04-14 16:23:53 +0800

    add NSWindow test

---
+++ NSWindow_Demo/NSWindow_Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NSWindow_Demo
+//
+//  Created by LeeWong on 2018/4/14.
+//  Copyright © 2018年 LeeWong. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
---
+++ NSWindow_Demo/NSWindow_Demo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  NSWindow_Demo
+//
+//  Created by LeeWong on 2018/4/14.
+//  Copyright © 2018年 LeeWong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    // Do any additional setup after loading the view.
+}
+
+
+- (void)setRepresentedObject:(id)representedObject {
+    [super setRepresentedObject:representedObject];
+
+    // Update the view, if already loaded.
+}
+
+
+@end
